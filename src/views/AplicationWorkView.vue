2fab5da4e58d19883c856edd4d26b5d807d6a295
HEANGYG <young.sengheang04@gmail.com>
2024-07-05 23:42:10 +0700

    update_code

--- src/views/AplicationWorkView.vue
+++ src/views/AplicationWorkView.vue
@@ -4,7 +4,9 @@
 
 <template>
     <div>
-            <h1 class="text-center pt-12">ព័ត៏មានថ្មីៗ</h1>
+            <h1 class="text-center pt-12 mb-5">កម្មវិធីការងារ</h1>
+            <h2 class="text-center">ថ្ងៃច័ន្ទ ៣ រោច ខែជេស្ព ឆ្នាំរោង ឆស័ក ពុទ្ធសករាជ ២៥៦៨ ថ្ងៃទី២៤ ខែមិថុនា ឆ្នាំ២០២៤</h2>
+            
     </div>
 </template>
 
@@ -14,5 +16,11 @@
           font-style: normal;
           font-weight: 300;
           font-size: xx-large;
+      }
+      h2{
+          font-family: 'Khmer OS Muol';
+          font-style: normal;
+          font-weight: 200;
+          font-size: x-large;
       }
 </style>
